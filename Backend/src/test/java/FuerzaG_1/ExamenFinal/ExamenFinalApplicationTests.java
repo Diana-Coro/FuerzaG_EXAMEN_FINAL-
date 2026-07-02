1ab1813337a00c197935928c009fe4f5f7c7a90b
Diana Coro Llave <esmeralda21diana@gmail.com>
2026-07-02 19:59:43 -0400

    Agregar Backend

---
+++ Backend/src/test/java/FuerzaG_1/ExamenFinal/ExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package FuerzaG_1.ExamenFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
